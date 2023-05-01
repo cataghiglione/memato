b029d41775bb3936ce9b11ba28e21899221d0227
Parrandos <63514959+Parrandos@users.noreply.github.com>
2023-04-30 22:06:00 -0300

    Resizing main pick team

--- ui-mio/src/pages/HomePage.jsx
+++ ui-mio/src/pages/HomePage.jsx
@@ -45,11 +45,11 @@ export function HomePage(props){
                     </h1>
                 </div>
                 <h1>My teams</h1>
-                <button className={"newTeamButton"} onClick={goToNewTeam}>
+                <button className={"newTeamButton"} onClick={goToNewTeam} style={{width:"325px"}}>
                     {teams.length===0 ? 'Create your first team' : 'New Team'}
                 </button>
                 {teams.length > 0 &&
-                    <select className={"team-pick"} multiple={true} onChange={changeNextTeam}>
+                    <select className={"team-pick"} multiple={true} onChange={changeNextTeam} style={{width:"325px"}}>
                         {teams.map(team =>
                                 <option className={"team-select-option"} value={team.id} style={{ textTransform: 'capitalize'}}>
                                     Nombre: {team.name}, Deporte: {team.sport} {team.quantity} </option>
