c0faf73b403e8dda5dd44a2c88de800c7b28f6da
constanzalasarte <constanza.lasarte@ing.austral.edu.ar>
2023-05-17 11:39:48 -0300

    teamId fixes

--- ui-mio/src/App.js
+++ ui-mio/src/App.js
@@ -36,6 +36,9 @@ const App = () =>{
     useEffect(() => {
         localStorage.setItem('teamId', JSON.stringify(teamId));
     }, [teamId]);
+    useEffect(() => {
+        localStorage.setItem('teamId', 0);
+    })
 
 
     const toggleTeamId = (value) => {
