3da5459bae9f4e718724cb20e99072f9006337a4
constanzalasarte <constanza.lasarte@ing.austral.edu.ar>
2023-06-13 21:27:09 -0300

    NotificationsCenter.jsx

--- ui-mio/src/pages/TopBar/NotificationsCenter.jsx
+++ ui-mio/src/pages/TopBar/NotificationsCenter.jsx
@@ -24,7 +24,7 @@ export function NotificationsCenter(props){
     }
 
     function goToMessages() {
-        return undefined;
+        navigate("/chat")
     }
 
     function seeAllNotifications() {
