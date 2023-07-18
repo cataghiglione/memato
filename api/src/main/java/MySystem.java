499eb13ead37db516d5389da024eb98279b028aa
constanzalasarte <constanza.lasarte@ing.austral.edu.ar>
2023-07-18 15:55:51 -0300

    fixes

--- api/src/main/java/MySystem.java
+++ api/src/main/java/MySystem.java
@@ -63,7 +63,12 @@ public class MySystem {
         return runInTransaction(datasource -> {
             final Searches searches = datasource.searches();
             try {
-                return searches.exists(Long.toString(team.getId()), time.getIntervals(), form.getDate(), form.getLatitude(), form.getLongitude(),form.getAge(), form.isRecurring()) ? searches.reactivateSearch(team, time.getIntervals(), form.getDate(), form.getLatitude(), form.getLongitude(), form.getAge(), form.isRecurring()) : Optional.of(searches.createSearch(team, form.getDate(), time.getIntervals(), form.getLatitude(), form.getLongitude(), form.getAge(), form.isRecurring()));
+                return searches.exists(Long.toString(team.getId()), time.getIntervals(), form.getDate(), form.getLatitude(), form.getLongitude(),
+                        form.getAge(), form.isRecurring()) ?
+                        searches.reactivateSearch(team, time.getIntervals(), form.getDate(), form.getLatitude(), form.getLongitude(),
+                                form.getAge(), form.isRecurring())
+                        : Optional.of(searches.createSearch(team, form.getDate(), time.getIntervals(), form.getLatitude(), form.getLongitude(),
+                        form.getAge(), form.isRecurring()));
             } catch (ParseException e) {
                 throw new RuntimeException(e);
             }
